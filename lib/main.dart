1336a829f7dfeb9fafedeee528afb44162aedc12
robertlagonoy09 <robertlagonoy09@gmail.com>
2026-07-31 12:25:39 +0800

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Robert NFT Marketplace',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
